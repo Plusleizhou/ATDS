44d92249be1c059c86484977080250a35dfd7e9a
shengjie5 <shengjie.wu@plus.ai>
2022-06-24 17:40:21 +0800

    add multiprocessing

--- data/data_utils/data_cleaning.py
+++ data/data_utils/data_cleaning.py
@@ -1,8 +1,11 @@
 import os
 import argparse
+import time
 import numpy as np
 import pandas as pd
+import multiprocessing
 from tqdm import tqdm
+from joblib import Parallel, delayed
 from torch.utils.data import Dataset
 
 
@@ -32,7 +35,12 @@ class CleaningDataset(Dataset):
         ctrs = df_dict["GRAPH"]["ctrs"]
         feats = df_dict["GRAPH"]["feats"]
 
+        # Due to the same id of ego and surrounding agents, the length of agent's trajectory will more than 5s.
+        # Remove the data which has the abnormal trajectories.
+        # Remove the data which have nan in trajectories or graph
         if trajs.shape[1] != 50:
+            self.remove_files.append(seq_id)
+        elif np.isnan(trajs.max()) or np.isnan(trajs.min()):
             self.remove_files.append(seq_id)
         elif max(np.abs(trajs.max()), np.abs(trajs.min())) > 500:
             self.remove_files.append(seq_id)
@@ -54,18 +62,40 @@ def remove_files(files, path):
             print("file: {} not found".format(path + file + "_argo.pkl"))
 
 
-def main(path):
-    dataset = CleaningDataset(path)
-    loop = tqdm(range(len(dataset)), total=len(dataset), leave=True)
+def search_for_bad_case(dataset, start_idx, batch_size, path):
+    loop = tqdm(range(batch_size), total=batch_size, leave=True)
     for i in loop:
-        count, seq_id = dataset[i]
-        loop.set_description(f"current file: {seq_id}")
-        loop.set_postfix_str(f"count: {count}")
+        if i + start_idx < len(dataset):
+            count, seq_id = dataset[i + start_idx]
+            loop.set_description(f"current file: {seq_id}")
+            loop.set_postfix_str(f"count: {count}")
     remove_files(dataset.remove_files, path)
 
 
-if __name__ == "__main__":
+def main(args):
+    start = time.time()
+
+    path = args.path
+    dataset = CleaningDataset(path)
+    num_files = len(dataset)
+    print("num of files: ", num_files)
+
+    n_proc = multiprocessing.cpu_count() - 2 if not args.debug else 1
+    batch_size = np.max([int(np.ceil(num_files / float(n_proc))), 1])
+    print('n_proc: {}, batch_size: {}'.format(n_proc, batch_size))
+
+    Parallel(n_jobs=n_proc)(delayed(search_for_bad_case)(dataset, i, batch_size, path)
+                            for i in range(0, num_files, batch_size))
+    print("Preprocess for {} set completed in {} minutes".format(args.path, (time.time() - start) / 60.0))
+
+
+def get_args():
     parser = argparse.ArgumentParser(description='Data Cleaning')
     parser.add_argument('--path', default='./data/features/train/', type=str, help='the path of saved features')
+    parser.add_argument('--debug', action="store_true", default=False, help='whether to debug')
     args = parser.parse_args()
-    main(args.path)
+    return args
+
+
+if __name__ == "__main__":
+    main(get_args())
